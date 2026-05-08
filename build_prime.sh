362266584f9b341166d2b0ff7de80abdbd5b2b97
codebymian <codebymian@gmail.com>
2026-05-08 10:56:37 +0500

    fixed ksu susfs impliment

--- build_prime.sh
+++ build_prime.sh
@@ -17,7 +17,7 @@ build_kernel() {
 
     # NO_QCACLD, CNSS_WORKAROUND, SEC_PCIE*: see vendor/samsung/${DEVICE}.config
     cat arch/arm64/configs/vendor/kona-sec-perf_defconfig arch/arm64/configs/vendor/samsung/${DEVICE}.config \
-        arch/arm64/configs/ksu.config arch/arm64/configs/vendor/not/no_werror.config \
+        arch/arm64/configs/vendor/not/no_werror.config \
         arch/arm64/configs/vendor/debugfs.config > arch/arm64/configs/temp_defconfig
 
     echo "
